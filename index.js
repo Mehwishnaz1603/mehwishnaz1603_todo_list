a43a52d975697e36de16fc663fc80188c866a6a5
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-05-01 22:51:37 +0500

    ..

--- index.js
+++ index.js
@@ -1,171 +1,79 @@
 #! /usr/bin/env node
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let conditions = true;
+console.log(chalk.bold.magenta('Welcome To _Todo-List App'));
+let Todo = async () => {
+    while (conditions) {
+        let option = await inquirer.prompt([
+            {
+                name: "choices", type: "list",
+                message: chalk.italic.yellowBright("Select an option you want todo: "),
+                choices: ["Add Task", "Delete Task", "Update Task", "View Todo-List", "Exit"]
             }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+        ]);
+        if (option.choices === "Add Task") {
+            await addTask();
+        }
+        else if (option.choices === "Delete Task") {
+            await deleteTask();
+        }
+        else if (option.choices === "Update Task") {
+            await updateTask();
+        }
+        else if (option.choices === "View Todo-List") {
+            await viewTask();
+        }
+        else if (option.choices === "Exit") {
+            conditions = false;
+        }
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var chalk_1 = require("chalk");
-var todoList = [];
-var conditions = true;
-console.log("<<<=>>>  ".concat(chalk_1.default.bold.magenta('Welcome To _Todo-List App'), "  <<<=>>>\n"));
-var main = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var option;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                if (!conditions) return [3 /*break*/, 11];
-                return [4 /*yield*/, inquirer_1.default.prompt([
-                        {
-                            name: "choices", type: "list",
-                            message: chalk_1.default.italic.yellowBright("Select an option you want to do: "),
-                            choices: ["Add Task", "Delete Task", "Update Task", "View Todo-List", "Exit"]
-                        }
-                    ])];
-            case 1:
-                option = _a.sent();
-                if (!(option.choices === "Add Task")) return [3 /*break*/, 3];
-                return [4 /*yield*/, addTask()];
-            case 2:
-                _a.sent();
-                return [3 /*break*/, 10];
-            case 3:
-                if (!(option.choices === "Delete Task")) return [3 /*break*/, 5];
-                return [4 /*yield*/, deleteTask()];
-            case 4:
-                _a.sent();
-                return [3 /*break*/, 10];
-            case 5:
-                if (!(option.choices === "Update Task")) return [3 /*break*/, 7];
-                return [4 /*yield*/, updateTask()];
-            case 6:
-                _a.sent();
-                return [3 /*break*/, 10];
-            case 7:
-                if (!(option.choices === "View Todo-List")) return [3 /*break*/, 9];
-                return [4 /*yield*/, viewTask()];
-            case 8:
-                _a.sent();
-                return [3 /*break*/, 10];
-            case 9:
-                if (option.choices === "Exit") {
-                    conditions = false;
-                }
-                _a.label = 10;
-            case 10: return [3 /*break*/, 0];
-            case 11: return [2 /*return*/];
-        }
-    });
-}); };
 // Function to add new task to the list 
-var addTask = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var newTask;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, inquirer_1.default.prompt([
-                    {
-                        name: "task",
-                        type: "input",
-                        message: chalk_1.default.italic.bgCyan.red("Enter your new task: "),
-                    }
-                ])];
-            case 1:
-                newTask = _a.sent();
-                todoList.push(newTask.task);
-                console.log(chalk_1.default.italic.green.underline("\n ".concat(newTask.task, " Task Added Successfully In Todo-List\n")));
-                return [2 /*return*/];
+let addTask = async () => {
+    let newTask = await inquirer.prompt([
+        {
+            name: "task", type: "input", message: chalk.italic.bgCyan.red("Enter your new task: "),
         }
-    });
-}); };
+    ]);
+    todoList.push(newTask.task);
+    console.log(chalk.italic.green.underline(`\n ${newTask.task} Task Added Successfully In Todo-List\n`));
+};
 // Function to view all Todo-List Tasks
-var viewTask = function () {
-    console.log(chalk_1.default.bold.underline.magenta("\n My Todo-List: \n"));
-    todoList.forEach(function (task, index) {
-        console.log(chalk_1.default.italic.underline.magenta.bold("".concat(index + 1, ": ").concat(task, "\n")));
+let viewTask = () => {
+    console.log(chalk.bold.underline.magenta("\n My Todo-List: \n"));
+    todoList.forEach((task, index) => {
+        console.log(chalk.italic.underline.magenta.bold(`${index + 1}: ${task}\n`));
     });
 };
 // Function to delete a task from the list 
-var deleteTask = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var taskIndex, deleteTask;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, viewTask()];
-            case 1:
-                _a.sent();
-                return [4 /*yield*/, inquirer_1.default.prompt([
-                        {
-                            name: "index",
-                            type: "number",
-                            message: chalk_1.default.italic.bgCyan.red("Enter the 'index no. ' of the task you want to delete: "),
-                        }
-                    ])];
-            case 2:
-                taskIndex = _a.sent();
-                deleteTask = todoList.splice(taskIndex.index - 1, 1);
-                console.log(chalk_1.default.italic.green.underline("\n ".concat(deleteTask, " This task has been deleted successfully from your Todo-List.\n")));
-                return [2 /*return*/];
+let deleteTask = async () => {
+    await viewTask();
+    let taskIndex = await inquirer.prompt([
+        {
+            name: "index", type: "number",
+            message: chalk.italic.bgCyan.red("Enter the 'index no. ' of the task you want to delete: "),
         }
-    });
-}); };
+    ]);
+    let deleteTask = todoList.splice(taskIndex.index - 1, 1);
+    console.log(chalk.italic.green.underline(`\n ${deleteTask} This task has been deleted successfully from your Todo-List.\n`));
+};
 // Function to update a task
-var updateTask = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var update_Task_index;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, viewTask()];
-            case 1:
-                _a.sent();
-                return [4 /*yield*/, inquirer_1.default.prompt([
-                        {
-                            name: "index",
-                            type: "number",
-                            message: chalk_1.default.italic.bgCyan.red("Enter the 'index no.' of the task you want to update: ")
-                        },
-                        {
-                            name: "new_task",
-                            type: "input",
-                            message: chalk_1.default.italic.yellowBright("Now Enter new task name: "),
-                        }
-                    ])];
-            case 2:
-                update_Task_index = _a.sent();
-                todoList[update_Task_index.index - 1] = update_Task_index.new_task;
-                console.log(chalk_1.default.italic.green.underline("\n Task at index no. ".concat(update_Task_index.index - 1, " updated successfully [For updated list check option: \"View Todo-List\" ]")));
-                return [2 /*return*/];
+let updateTask = async () => {
+    await viewTask();
+    let update_Task_index = await inquirer.prompt([
+        {
+            name: "index", type: "number",
+            message: chalk.italic.bgCyan.red("Enter the 'index no.' of the task you want to update: ")
+        },
+        {
+            name: "new_task", type: "input",
+            message: chalk.italic.yellowBright("Now Enter new task name: "),
         }
-    });
-}); };
-main();
+    ]);
+    todoList[update_Task_index.index - 1] = update_Task_index.new_task;
+    console.log(chalk.italic.green.underline(`\n Task at index no. ${update_Task_index.index - 1} 
+    updated successfully [For updated list check option: "View Todo-List" ]`));
+};
+Todo();
